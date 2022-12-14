8705a1c11910c58bba0f4636d6600b442c906595
ArAs101 <armin.assadzadeh@stud.fh-campuswien.ac.at>
2022-12-14 15:36:32 +0100

    Erweiterung des Hauptmenüs und Behandlung von ungültigen Eingaben (braucht noch Überarbeitung...) Änderungen an der Klasse Players

--- src_Console/Players.java
+++ src_Console/Players.java
@@ -1,14 +1,24 @@
 package src_Console;
 
 public class Players {
-    private String name;
-    private char symbol;
+    public String name;
+    public char symbol;
+/*
+    public Players p1 = new Players(name, symbol);
+    public Players p2 = new Players(name, symbol);
 
+
+ */
     public Players(String name, char symbol) {
-        this.name = name;
-        this.symbol = symbol;
+
     }
 
 
+    public String getName() {
+        return name;
+    }
 
+    public char getSymbol() {
+        return symbol;
+    }
 }
